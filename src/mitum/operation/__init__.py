85292aa6486cc928a19de3c6044b96fa73b8bdfa
wyuinche <wyuinche.mido@gmail.com>
2021-05-25 15:48:32 +0900

    Change package name

--- src/mitum/operation/__init__.py
+++
@@ -1,5 +0,0 @@
-from mitum.operation.base import (Address, Amount, FactSign, Memo, Operation,
-                                  OperationBody, OperationFact,
-                                  OperationFactBody)
-
-__all__ = ['generate_create_accounts', 'generate_key_updater', 'generate_transfers']
---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mitumc',
+    version='0.1',
+    description='mitum-currency python util',
+    author='pmj',
+    author_email='minju.park@socialinfratech.com',
+    license="",
+    packages=['mitumc'],
+    url="https://github.com/ProtoconNet/mitum-py-util",
+    install_requires=['']
+)
