0b88a2921c9129dd0b9f32c06344d219d1f173ec
wyuinche <wyuinche.mido@gmail.com>
2021-05-26 17:21:19 +0900

    Add setup.py and comments

--- src/test/json/seal.py
+++ src/test/json/seal.py
@@ -1,22 +1,29 @@
+from mitumc.operation import key_updater
 from mitumc.operation.operations import generate_create_accounts, generate_key_updater, generate_seal, generate_transfers
 
-ac0_prv = "6169fa9d292b1d7dd5ea58df8e6968453bd23341cd8a4cab9cb03f33783890a3-0114:0.0.1"
-ac0_pub = "046482478b070090a8a85a33488b8147bd28a62bd666af30f8086fc5f9157ab5379b30e5d2b4f2264229d394f8f7fe5b7e7da27fd83e9613ae2ecc2ba6d99c1ae2-0115:0.0.1"
-ac0_addr = "7D1mSbbraYGEmQ13BCxGt9umFRhGqfoPUrJ7fp1f6fC4-a000:0.0.1"
+ac0_prv = "L5GTSKkRs9NPsXwYgACZdodNUJqCAWjz2BccuR4cAgxJumEZWjok-0112:0.0.1"
+ac0_pub = "rcrd3KA2wWNhKdAP8rHRzfRmgp91oR9mqopckyXRmCvG-0113:0.0.1"
+ac0_addr = "8PdeEpvqfyL3uZFHRZG5PS3JngYUzFFUGPvCg29C2dBn-a000:0.0.1"
 
-ac1_prv = "24cd3a9934b6d299aa0b80135d629021e3104affbf91e0008fcfc7abcec0042d-0114:0.0.1"
-ac1_pub = "0405256e5caedf76215c25f89c970d10b1b0c7b5314b55d3a4aac093d1ba5aeb60a5f831cb8d0da4a88945f631dff6ceb1b581eeea8e45c3667c1d4d50f8fe07d0-0115:0.0.1"
-ac1_addr = "DFAQ9V8ZkMnTJCvHocCZjP7TPEQyCaq9tFkC9pKWCYAe-a000:0.0.1"
+# ac0_prv = "SBGISVULOQA6BPEYF4OS2JGMBST7HYCBSL3TA2QRVGRNBMVWIZVE6336-0110:0.0.1"
+# ac0_pub = "GBYLIBJYZP6ZIYPFGOZSXSAPMRDA6XXRKNSMOMRCKNV2YZ35DGRPEQ35-0111:0.0.1"
+# ac0_addr= "8HQt6CfBVgMhLmPxcataTF2CXHuw2Km32FAcW7FXmQZ3-a000:0.0.1"
 
-createAccounts = generate_create_accounts(
-    "mitum", ac0_prv, ac0_addr, (100, "MCC"), [(ac1_pub, 100)]
-)
+ac1_prv = "8d15c09377e0d504f175f8ad595690b83e59e08c67d7f71f7795a489412b6f04-0114:0.0.1"
+ac1_pub = "04c7a0b69c4041d2d3cf60d9318b5fdb1c29c7f63b3514aab52db6a852083dd3e1065afa8524c4ba54688ae36055377b2bb3de931054c124f01f38e7eab27e9e8f-0115:0.0.1"
+ac1_addr = "8dsqP9dUPKv3TjJg6DCKJ7NE7vsMx47Gc4VrseEcyXtt-a000:0.0.1"
 
-keyUpdater = generate_key_updater("mitum", ac0_prv, ac0_addr, ac1_pub, 100, "MCC")
+# ac1_pub = "GCQWMULB2DP6C7435BYAJDTPEC2T3F3JW3YTPPSFE6DAPZHW6SBXJIQS-0111:0.0.1"
+
+# createAccounts = generate_create_accounts(
+#     "mitum", ac0_prv, ac0_addr, (100, "MCC"), [(ac1_pub, 100)]
+# )
+
+# keyUpdater = generate_key_updater("mitum", ac0_prv, ac0_addr, ac1_pub, 100, "MCC")
 
 transfers = generate_transfers("mitum", ac0_prv, ac0_addr, ac1_addr, (100, "MCC"))
 
 operations = [transfers]
 
 generate_seal("./test/json/seal.json", "mitum", ac0_prv, operations)
-createAccounts.to_json("./test/json/create_accounts.json")
+transfers.to_json("./test/json/transfers.json")
